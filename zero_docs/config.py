ada74f376e4dd4cd8b5d8bba7d585478984998cc
Gianfranco Demarco <gianfrademarco@gmail.com>
2024-02-11 17:45:55 +0100

    fix: prompt

--- zero_docs/config.py
+++ zero_docs/config.py
@@ -3,7 +3,11 @@ import os
 MODEL = os.getenv("MODEL", "gpt-3.5-turbo-0125")
 ZERO_DOCS_INPUT_PATHS = os.getenv("ZERO_DOCS_INPUT_PATHS", ".")
 ZERO_DOCS_CODE_ENTITIES = os.getenv("CODE_ENTITIES", "module,class,function").split(",")
+
+_prompt = "You are an AI assistant that generates docstrings for Python code. Be as concise and pythonic as possible.\n\nReturn ONLY the docstring text, no markdown syntax.\n\nGenerate the docstring for this {code_entity}:"
 PROMPT = os.getenv(
     "PROMPT",
-    "You are an AI assistant that generates docstrings for Python code. Be as concise and pythonic as possible.\n\nReturn ONLY the docstring text, no markdown syntax.\n\nGenerate the docstring for this {code_entity}:"
+    _prompt
 )
+if not PROMPT:
+    PROMPT = _prompt
